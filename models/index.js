6eff55b1176620cec1413561ef6a6d14a76b8660
Kelsey Duru <kdassignments@gmail.com>
2021-11-16 20:11:13 -0800

    playing around with insomnia and testing, coding errors fixed, parse errors as well

--- models/index.js
+++ models/index.js
@@ -1,29 +1,43 @@
-const { Model, DataTypes } = require('sequelize');
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-const sequelize = require('../config/connection.js');
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
-class Category extends Model {}
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-Category.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+  //as: 'product_tags',
+  foreignKey: 'product_id'
+})
 
-module.exports = Category;
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  //as: 'tagged_products',
+  foreignKey: 'tag_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
